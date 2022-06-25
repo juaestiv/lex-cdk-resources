97bd48e2583f65d456e14f7e565d3b71375c8014
juaestiv <juaestiv@gmail.com>
2022-06-25 13:50:20 +0200

    Initial commit

---
+++ test/multi-bot-utilities.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MultiBotUtilities from '../lib/multi-bot-utilities-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/multi-bot-utilities-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MultiBotUtilities.MultiBotUtilitiesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
